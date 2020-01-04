beadd2acd098a62be7a069d638adef55b3417f3b
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-04 17:51:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
   counter = 0 
-  newarr = []
   while counter < array.length do
-    newarr[counter] = array[counter] ** 2 
+    arr[counter] = array[counter] ** 2 
   
     counter += 1 
   end
-  puts newarray
+  puts array
 end
